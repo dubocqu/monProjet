e5c4a4c84d13b1d1233773550ae9812cc948763f
Quentin Duboc <quentinduboc3@gmail.com>
2020-12-08 15:37:26 +0100

    Modification du server

--- server.js
+++ server.js
@@ -2,11 +2,17 @@ let express = require('express');
 let app = express();
 let port = 3000;
 
+
+//Préparation du server
+app.use(express.static(__dirname + '/www'));
+
 app.listen(port, () => {
     console.log('Le serveur est en route');
     console.log(`Serveur listening at http://localhost:${port}`);
 })
 
+
+//Routes
 app.get('/', (req, res, next) => {
-    res.send('Bonjour ! ');
+    res.send('www/index.html');
 });
