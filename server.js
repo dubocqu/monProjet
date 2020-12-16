0501df03764097a439ad172352a8b5af3392b095
Quentin Duboc <quentinduboc3@gmail.com>
2020-12-16 08:48:39 +0100

    ajout d'un menu bootstrap

--- server.js
+++ server.js
@@ -5,7 +5,10 @@ let port = 3000;
 
 //Préparation du server
 app.use(express.static(__dirname + '/www'));
-
+app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js'));
+app.use('/js', express.static(__dirname + '/node_modules/jquery/dist'));
+app.use('/js', express.static(__dirname + '/node_modules/popper.js/dist/umd'));
+app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
 app.listen(port, () => {
     console.log('Le serveur est en route');
     console.log(`Serveur listening at http://localhost:${port}`);
